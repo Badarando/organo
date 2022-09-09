0f8d60dd60bdac3dedc5e65e4c115dbe18740755
Marcos Paulo Badaró <marcospaulobadaro@gmail.com>
2022-09-08 22:52:09 -0300

    continuando o projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Banner from './components/Banner/Banner';
+import { Banner } from './components/Banner';
+import { CampoTexto } from './components/CampoTexto';
+import { Formulario } from './components/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Formulario />
     </div>
   );
 }
 
 export default App;
+ 

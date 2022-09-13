c328462f3a86ac58f8fe1b85ec61213622b4c865
Marcos Paulo Badaró <marcospaulobadaro@gmail.com>
2022-09-13 16:25:03 -0300

    continuando projeto

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -2,11 +2,11 @@ import './Colaborador.css';
 
 export const Colaborador = () => {
     return (
-        <div>
-            <div>
+        <div className='colaborador'>
+            <div className='cabecalho'>
                 <img src='https://github.com/badarando.png' alt='Marcos Badaró'/>
             </div> 
-            <div>
+            <div className='rodape'>
                 <h4>Marcos Badaró</h4>
                 <h5>Aluno</h5>
             </div>

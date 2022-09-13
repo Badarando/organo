d2adb7225d976be9b8ba4af9231aefa0e07a6e17
Marcos Paulo Badaró <marcospaulobadaro@gmail.com>
2022-09-13 18:24:09 -0300

    começando parte final

--- src/App.js
+++ src/App.js
@@ -47,14 +47,20 @@ function App() {
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
     console.log(colaborador)
-    setColaboradores(...colaboradores, colaborador)
+    setColaboradores([...colaboradores, colaborador])
   }
-
+ 
   return (
     <div className="App">
       <Banner />
       <Formulario times={times.map( time => time.nome)} aoColaboradorCadastrado={aoNovoColaboradorAdicionado}/>
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
+      {times.map(time => <Time
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}
     </div>
   );
 }
--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './Colaborador.css';
 
-export const Colaborador = () => {
+export const Colaborador = ({ nome, imagem, cargo }) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='https://github.com/badarando.png' alt='Marcos Badaró'/>
+                <img src={imagem} alt={nome}/>
             </div> 
             <div className='rodape'>
-                <h4>Marcos Badaró</h4>
-                <h5>Aluno</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
     )
